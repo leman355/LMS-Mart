343bf999cce437b2ee6f891fe848a99a3bed7e24
leman355 <leman355@gmail.com>
2022-05-08 04:20:15 +0400

    header end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+var prevScrollpos = window.pageYOffset;
+
+/* Get the header element and it's position */
+var headerDiv = document.querySelector("header");
+var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  /* if scrolling down, let it scroll out of view as normal */
+  if (prevScrollpos <= currentScrollPos ){
+      headerDiv.classList.remove("fixedToTop");
+      headerDiv.style.top ="-7.2rem";
+  }
+  /* otherwise if we're scrolling up, fix the nav to the top */
+  else{  
+      headerDiv.classList.add("fixedToTop");
+      headerDiv.style.top = "0";
+  }
+
+  prevScrollpos = currentScrollPos;
+}
+
+
+
 let bars = document.querySelector("#header .myContainer .bars");
 let barsF = document.querySelector("#header .myContainer .bars .barsF");
 let iks = document.querySelector("#header .myContainer .bars .iks");
@@ -113,3 +138,4 @@ for (let i = 0; i < topshowI.length; i++) {
     topshowI[i].classList.add("activeI");
   });
 }
+
